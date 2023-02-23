ea321bab5e5e2c2f837fc544b9d0d1a4c2230e19
Diego Arias <dariascauas@gmail.com>
2023-02-23 20:16:22 -0300

    competitive programming compile alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,5 @@ set -U fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+alias zcc="g++ -Wall -Wextra -pedantic -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -Wno-unknown-pragmas -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -fstack-protector -o z"
