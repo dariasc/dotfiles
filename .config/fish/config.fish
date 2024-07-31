9188bfa4d664633bbbc08a7bdf5587cede6813d6
Diego Arias <dariascauas@gmail.com>
2024-07-31 13:28:11 -0400

    chore: initial river commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+set -U fish_greeting
+set -U lucid_skip_newline
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+export PATH="$PATH:$HOME/.local/bin/"
+
